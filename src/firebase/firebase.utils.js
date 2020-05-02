64224d204abdfa540eb84264a59f864c0e26b138
Anie Edem <anieedem@Anies-MacBook-Air.local>
2020-05-02 20:18:17 +0200

    Integrated firebase sign in and sign out

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+
+const config = {
+    
+        apiKey: "AIzaSyCUjrcy19YS-KszoFPpoNl5UlIgqCJA1XM",
+        authDomain: "crown-db-b3275.firebaseapp.com",
+        databaseURL: "https://crown-db-b3275.firebaseio.com",
+        projectId: "crown-db-b3275",
+        storageBucket: "crown-db-b3275.appspot.com",
+        messagingSenderId: "823893514527",
+        appId: "1:823893514527:web:95b4c0e7f16e58eb2ef82a",
+        measurementId: "G-YC0RY056X8"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
