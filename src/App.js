6814258ae6952ca993f9d15e45097b05e323fed9
Anie Edem <anieedem@Anies-MacBook-Air.local>
2020-05-03 13:23:01 +0200

    Implenenting Sign up ppage and integrating auth as well as database addition in firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,10 +20,28 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      
+    if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapshot => {
+         this.setState({
+            
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+
+         });
+        
+        console.log(this.state)
+        });
+
+        
+    } else {
+      this.setState({currentUser: userAuth});
+    }
     });
   }
   componentWillUnmount() {
--- src/components/sign-in/signin.jsx
+++ src/components/sign-in/signin.jsx
@@ -39,7 +39,7 @@ class SignIn extends React.Component {
                     type="email" 
                     value={this.state.email}
                     handleChange={this.handleChange}
-                    label="email"
+                    label="Email"
                     required/>
                     
                     
@@ -48,7 +48,7 @@ class SignIn extends React.Component {
                     type="password" 
                     value={this.state.password} 
                     handleChange={this.handleChange}
-                    label = "password"
+                    label = "Password"
                     required/>
 
                     <div className="buttons">
