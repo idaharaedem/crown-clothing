7acbc0f8f429b353f67bc1acb2156006eed7bdd3
Anie Edem <anieedem@Anies-MacBook-Air.local>
2020-05-05 12:00:12 +0200

    Adding redux to cart icon and implementing

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
 import {connect} from 'react-redux';
 
 
+
+
 import './header.styles.scss'
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
  <div className="header">
    <Link className="logo-container" to="/">
         <Logo className="logo"/>
@@ -27,13 +31,22 @@ const Header = ({currentUser}) => (
         <Link className="option" to="/signin">
         SIGN IN    
         </Link>}
-    </div>
 
+        <CartIcon/>
+    </div>
+    {
+        hidden 
+        ?
+        ''
+        :
+        <CartDropdown/>
+    }
  </div>   
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
